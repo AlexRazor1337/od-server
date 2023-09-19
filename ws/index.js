358c1f33cdc415b7c2faa5534f19ed97ccb28d84
AlexRazor <alexrazor1337@gmail.com>
2023-09-19 21:58:20 +0300

    Lint+format

--- ws/index.js
+++ ws/index.js
@@ -1,55 +1,57 @@
-import { io } from '../server.js'
+import { io } from '../server.js';
 
 const players = {};
 let world = {};
 
 const initGame = () => {
-    world = {}
-}
+  world = {};
+};
 
 setInterval(() => {
-    if (Object.keys(players).length > 0) {
-        io.to('game').emit('tick', players);
-    }
+  if (Object.keys(players).length > 0) {
+    io.to('game').emit('tick', players);
+  }
 }, 3);
 
 io.on('connect', (socket) => {
-    let player = {};
-    socket.on('init', (data, callback) => {
-        socket.join('game');
-        player = {
-            id: socket.id,
-            name: data.playerName,
-            worldX: 6 * 48,
-            worldY: 6 * 48,
-        };
-        players[data.playerName] = player;
+  let player = {};
+  socket.on('init', (data, callback) => {
+    socket.join('game');
+    player = {
+      id: socket.id,
+      name: data.playerName,
+      worldX: 6 * 48,
+      worldY: 6 * 48,
+    };
+    players[data.playerName] = player;
 
-        callback({
-           player,
-           players,
-           world,
-        });
+    callback({
+      player,
+      players,
+      world,
     });
+  });
 
-    socket.on('client-tick', (data) => {
-        console.log('client-tick', data)
-        players[player.name].worldX = data.worldX;
-        players[player.name].worldY = data.worldY;
-        players[player.name].direction = data.direction;
-        players[player.name].frameX = data.frameX;
-        players[player.name].currentState = data.currentState;
+  socket.on('client-tick', (data) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('client-tick', data);
+    }
+    players[player.name].worldX = data.worldX;
+    players[player.name].worldY = data.worldY;
+    players[player.name].direction = data.direction;
+    players[player.name].frameX = data.frameX;
+    players[player.name].currentState = data.currentState;
 
-        // const capturedPlayerData = checkForPlayerCollisions(player.playerData, player.playerConfig, players, playersForUsers, socket.id);
-        // if (capturedPlayerData) {
-        //     io.to('game').emit('player-absorb', capturedPlayerData);
-        //     io.to('game').emit('update-leaderboard', getLeaderboard());
-        // }
-    });
+    // const capturedPlayerData = checkForPlayerCollisions(player.playerData, player.playerConfig, players, playersForUsers, socket.id);
+    // if (capturedPlayerData) {
+    //     io.to('game').emit('player-absorb', capturedPlayerData);
+    //     io.to('game').emit('update-leaderboard', getLeaderboard());
+    // }
+  });
 
-    socket.on('disconnect', (data) => {
-        delete players[player.name];
-    });
+  socket.on('disconnect', (data) => {
+    delete players[player.name];
+  });
 });
 
 initGame();
